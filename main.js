fac632d4072398163879d47eaa4b9bb73c0f51c1
KreutzerCode <KreutzerCode@gmail.com>
2022-05-01 22:10:33 +0200

    [TASK] adds persistent chat history of variable size

--- main.js
+++ main.js
@@ -1,31 +1,99 @@
 const Discord = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 const fs = require("fs");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 const client = new Discord.Client({
   intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
 });
+
+let { backstory, predefinedConversations, userConversations } = JSON.parse(fs.readFileSync('./chatData.json', 'utf8'));
+let chatHistoryString = "";
+const maxChatHistory = 100;
+
 client.login(process.env.DISCORD_TOKEN);
 client.on("ready", () => {
   console.log(
     `Clara Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels`
   );
-  client.user.setActivity(`hot and bored`);
 });
 
 client.on("messageCreate", async (message) => {
-
   if (message.author.bot) return;
   if (!message.content) return;
+  if (message.length < 2) return;
 
   message.channel.sendTyping();
 
-  const spawn = require("child_process").spawn;
-  const process = spawn("python", ["./GPT3-ChatBot.py", message.content]);
+  let botAnswer = await returnBotAnswer(message.content);
+  if (botAnswer.length < 1) {
+    botAnswer = "..."
+  } else {
+    addConversationToChatHistory(message.content, botAnswer);
+  };
 
-  process.stdout.on("data", (data) => {
-    let response = data.toString().trim();
-    if(response.length < 1) response = "...";
-    message.channel.send(response);
-  });
+  message.channel.send(botAnswer);
 });
+
+function addConversationToChatHistory(message, answer) {
+  chatHistoryString = chatHistoryString.concat("\nHuman: ", message, "\nAI: ", answer);
+
+  if (userConversations.length >= maxChatHistory) {
+    userConversations.shift();
+  }
+
+  userConversations.push({
+    human: message,
+    ai: answer
+  });
+
+  saveConversationsToJsonFile();
+}
+
+function saveConversationsToJsonFile() {
+  const data = {
+    backstory,
+    predefinedConversations,
+    userConversations,
+  }
+
+  // write file to disk
+  fs.writeFile('./chatData.json', JSON.stringify(data), 'utf8', (err) => {
+    if (err) {
+      console.log(`Error writing file: ${err}`);
+    } else {
+      console.log(`File is written successfully!`);
+    }
+  });
+}
+
+function createChatHistoryString() {
+  predefinedConversations.forEach(conversation => {
+    chatHistoryString = chatHistoryString.concat("\nHuman: ", conversation.human, "\nAI: ", conversation.ai);
+  });
+
+  userConversations.forEach(conversation => {
+    chatHistoryString = chatHistoryString.concat("\nHuman: ", conversation.human, "\nAI: ", conversation.ai);
+  });
+}
+
+async function returnBotAnswer(message) {
+  const response = await openai.createCompletion("text-davinci-002", {
+    prompt: backstory.concat(chatHistoryString || createChatHistoryString(), " Human: ", message, " AI:"),
+    temperature: 0.9,
+    max_tokens: 150,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0.6,
+    stop: [" Human:", " AI:"],
+  });
+
+  let responseText = response['data'].choices[0].text.toString().trim();
+
+  return responseText;
+}
